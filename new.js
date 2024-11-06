11c1d5c616414576ed7dbc61285d90252e94cc13
Shahbaz <jadoonshahbaz19@gmail.com>
2024-11-06 04:25:30 -0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,16 @@
+
+function add (a,b) {
+    return a+b;
+}
+const personData = {
+    aDD : add,
+    name : "Shahbaz Khan",
+    age : 22 ,
+    semester : 8,
+    Address : "Abbottabad",
+    
+}
+console.log(personData);
+module.exports = {
+   personData
+}
